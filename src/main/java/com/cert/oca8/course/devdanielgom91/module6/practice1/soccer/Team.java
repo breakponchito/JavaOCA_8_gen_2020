30337e8b00849e0f23f39d9b14e2981bb08d5cfb
devhomer <homer.jay.s77@gmail.com>
2020-06-17 13:59:44 -0500

    Se crearon las clases para las practicas

--- src/main/java/com/cert/oca8/course/devdanielgom91/module6/practice1/soccer/Team.java
+++ src/main/java/com/cert/oca8/course/devdanielgom91/module6/practice1/soccer/Team.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cert.oca8.course.devdanielgom91.module6.practice1.soccer;
 /**
  *
