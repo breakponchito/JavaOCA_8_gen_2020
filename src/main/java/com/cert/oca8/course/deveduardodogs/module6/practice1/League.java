84262f8fb14ac2980b259aad106ca915c89bcf46
weduardogs <eduardogs.1030@gmail.com>
2020-06-18 19:30:12 -0500

    Atención a los comentarios, código de acuerdo a la guía

--- src/main/java/com/cert/oca8/course/deveduardodogs/module6/practice1/League.java
+++ src/main/java/com/cert/oca8/course/deveduardodogs/module6/practice1/League.java
@@ -4,27 +4,27 @@ public class League {
     public static void main(String[] args) {
         //Creating team 1
         Player player1 = new Player();
-        player1.playerName = "Nicolas Otamendi";
+        player1.playerName = "George Eliot";
         Player player2 = new Player();
-        player2.playerName = "Lionel Messi";
+        player2.playerName = "Graham Greene";
         Player player3 = new Player();
-        player3.playerName = "Neymar Jr.";
+        player3.playerName = "Geoffrey Chaucer";
         Player[] players = {player1, player2, player3};
 
         Team team1 = new Team();
-        team1.teamName = "Java team";
+        team1.teamName = "The Greens";
         team1.playerArray = players;
 
         //Creating team 2
         Team team2 = new Team();
-        team2.teamName = "C team";
+        team2.teamName = "The Reds";
         team2.playerArray = new Player[3];
         team2.playerArray[0] = new Player();
-        team2.playerArray[0].playerName = "Cristiano Ronaldo";
+        team2.playerArray[0].playerName = "Robert Service";
         team2.playerArray[1] = new Player();
-        team2.playerArray[1].playerName = "Paulo Dybala";
+        team2.playerArray[1].playerName = "Robbie Burns";
         team2.playerArray[2] = new Player();
-        team2.playerArray[2].playerName = "Zlatan Ibrahimovic";
+        team2.playerArray[2].playerName = "Rafael Sabatini";
 
         //Printing team 1 players
         for (Player thePlayer: team1.playerArray){
--- src/main/java/com/cert/oca8/course/deveduardodogs/module6/practice2/League.java
+++ src/main/java/com/cert/oca8/course/deveduardodogs/module6/practice2/League.java
@@ -10,48 +10,47 @@ public class League {
     public static void main(String[] args) {
         //Team 1
         Player player1 = new Player();
-        player1.playerName = "Nicolas Otamendi";
+        player1.playerName = "George Eliot";
         Player player2 = new Player();
-        player2.playerName = "Lionel Messi";
+        player2.playerName = "Graham Greene";
         Player player3 = new Player();
-        player3.playerName = "Neymar Jr.";
-        Player [] players = {player1, player2, player3};
+        player3.playerName = "Geoffrey Chaucer";
+        Player[] players = {player1, player2, player3};
 
         Team team1 = new Team();
-        team1.teamName = "Java team";
+        team1.teamName = "The Greens";
         team1.playerArray = players;
 
-        //Team 2
+        //Creating team 2
         Team team2 = new Team();
-        team2.teamName = "C team";
+        team2.teamName = "The Reds";
         team2.playerArray = new Player[3];
         team2.playerArray[0] = new Player();
-        team2.playerArray[0].playerName = "Cristiano Ronaldo";
+        team2.playerArray[0].playerName = "Robert Service";
         team2.playerArray[1] = new Player();
-        team2.playerArray[1].playerName = "Paulo Dybala";
+        team2.playerArray[1].playerName = "Robbie Burns";
         team2.playerArray[2] = new Player();
-        team2.playerArray[2].playerName = "Zlatan Ibrahimovic";
+        team2.playerArray[2].playerName = "Rafael Sabatini";
 
         /* Practice 6-2. Create a Game here */
-        Game match1 = new Game();
-        match1.homeTeam = team1;
-        match1.awayTeam = team2;
+        Game currGame = new Game();
+        currGame.homeTeam = team1;
+        currGame.awayTeam = team2;
 
         /* Practice 6-2. Create a Goal object here */
         Goal goal = new Goal();
+        goal.thePlayer = player2;
+        goal.theTeam = team1;
+        goal.theTime = 55;
 
         /* Practice 6-2. Put  Goal object in a Goal array and assign Goal array to goals attribute of Game object */
-        Goal [] totalGoals = new Goal[1];
-        totalGoals[0] = goal;
-        totalGoals[0].thePlayer = player2;
-        totalGoals[0].theTeam = team1;
-        totalGoals[0].theTime = 91.52;
-        match1.goals = totalGoals;
+        currGame.goals = new Goal[] {goal};
 
         /* Practice 6-2. Print out the score of the Game */
-        System.out.println("This was the first match of the season and the score is: ");
-        System.out.println(match1.goals[0].theTeam.teamName + ": " + match1.goals.length + " - " + team2.teamName + ": " + 0);
-        System.out.println("Goal scored by " + match1.goals[0].thePlayer.playerName + " '" + match1.goals[0].theTime);
+        System.out.println("Goal scored after " +
+        currGame.goals[0].theTime + " mins by " +
+        currGame.goals[0].thePlayer.playerName + " of " +
+        currGame.goals[0].theTeam.teamName);
 
     }
 
