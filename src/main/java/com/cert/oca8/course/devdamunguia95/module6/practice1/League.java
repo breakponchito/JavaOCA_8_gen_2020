3e530b48d24d492d833cd138bf0a4f2933d368b6
DanielMunguia95 <damunguia95@gmail.com>
2020-06-19 21:35:55 -0500

    Se agrega Team1 y tema2 de practica 1

--- src/main/java/com/cert/oca8/course/devdamunguia95/module6/practice1/League.java
+++ src/main/java/com/cert/oca8/course/devdamunguia95/module6/practice1/League.java
@@ -23,9 +23,32 @@ public class League {
 
         Player[] thePlayers = {player1, player2, player3};
 
-       
+        Team team1 = new Team();
+        team1.teamName = "The Greens";
+        team1.playerArray = thePlayers;
 
-      
+        for (Player player : team1.playerArray) 
+        {
+            System.out.println(player.playerName);
+        }
+
+        //Second Team	
+        Team team2 = new Team();
+        team2.teamName = "The Reeds";
+        team2.playerArray = new Player[3];
+
+        team2.playerArray[0] = new Player();
+        team2.playerArray[0].playerName = "Robert Service";
+        team2.playerArray[1] = new Player();
+        team2.playerArray[1].playerName = "Robbie Burns";
+        team2.playerArray[2] = new Player();
+        team2.playerArray[2].playerName = "Rafael Sabatini";
+
+        for (Player thePlayer : team2.playerArray) 
+        {
+            System.out.println(thePlayer.playerName);
+
+        }
 
     }
 
